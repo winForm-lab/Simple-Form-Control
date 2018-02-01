3e7cb27b6169def8fb2cc4b5fd0a327592c2a996
ESSARRAJ <essarraj.fouad@gmail.com>
2018-02-01 20:27:52 +0100

    AttributesDemoControl

--- Simple-Form-Control/Form1.Designer.cs
+++ Simple-Form-Control/Form1.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.flashTrackBar1 = new Microsoft.Samples.WinForms.Cs.FlashTrackBar.FlashTrackBar();
             this.firstControl1 = new Simple_Form_Control.FirstControl();
+            this.attributesDemoControl1 = new AttributesDemoControlLibrary.AttributesDemoControl();
             this.SuspendLayout();
             // 
             // flashTrackBar1
@@ -37,38 +39,42 @@
             this.flashTrackBar1.BackColor = System.Drawing.Color.Black;
             this.flashTrackBar1.DarkenBy = ((byte)(218));
             this.flashTrackBar1.ForeColor = System.Drawing.Color.White;
-            this.flashTrackBar1.Location = new System.Drawing.Point(36, 133);
+            resources.ApplyResources(this.flashTrackBar1, "flashTrackBar1");
             this.flashTrackBar1.Name = "flashTrackBar1";
-            this.flashTrackBar1.Size = new System.Drawing.Size(289, 58);
-            this.flashTrackBar1.TabIndex = 3;
-            this.flashTrackBar1.Text = "flashTrackBar1";
             this.flashTrackBar1.Value = 72;
             // 
             // firstControl1
             // 
-            this.firstControl1.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.firstControl1, "firstControl1");
             this.firstControl1.Name = "firstControl1";
-            this.firstControl1.Size = new System.Drawing.Size(249, 53);
-            this.firstControl1.TabIndex = 2;
-            this.firstControl1.Text = "sss";
             this.firstControl1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // attributesDemoControl1
+            // 
+            this.attributesDemoControl1.AlertForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.attributesDemoControl1, "attributesDemoControl1");
+            this.attributesDemoControl1.DataMember = "";
+            this.attributesDemoControl1.DataSource = null;
+            this.attributesDemoControl1.Name = "attributesDemoControl1";
+            this.attributesDemoControl1.Threshold = null;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 269);
+            this.Controls.Add(this.attributesDemoControl1);
             this.Controls.Add(this.flashTrackBar1);
             this.Controls.Add(this.firstControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private FirstControl firstControl1;
         private Microsoft.Samples.WinForms.Cs.FlashTrackBar.FlashTrackBar flashTrackBar1;
+        private AttributesDemoControlLibrary.AttributesDemoControl attributesDemoControl1;
     }
 }
 
