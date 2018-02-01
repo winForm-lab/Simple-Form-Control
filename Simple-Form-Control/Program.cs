24c4c4dc843c6c6b0a1804f71d9939bbcca48744
ESSARRAJ <essarraj.fouad@gmail.com>
2018-02-01 20:41:34 +0100

    CustomControls Test

--- Simple-Form-Control/Program.cs
+++ Simple-Form-Control/Program.cs
@@ -16,7 +16,7 @@ namespace Simple_Form_Control
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Simple_Form_Control.CustomControls.Form1());
         }
     }
 }
