3cf5f6b906ff510cae5e6fff7ac2a8f4808e8640
ESSARRAJ <essarraj.fouad@gmail.com>
2018-02-01 19:58:30 +0100

    Init comit

---
+++ Simple-Form-Control/FirstControl.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Form_Control
+{
+
+    // https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-develop-a-simple-windows-forms-control
+    public class FirstControl : Control
+    {
+
+        public FirstControl()
+        {
+            
+        }
+
+        public override string Text {
+            get => base.Text;
+            set
+            {
+                base.Text = value;
+                Invalidate();
+            }
+
+        }
+
+
+
+        // ContentAlignment is an enumeration defined in the System.Drawing
+        // namespace that specifies the alignment of content on a drawing 
+        // surface.
+        private ContentAlignment alignmentValue = ContentAlignment.MiddleLeft;
+
+        [
+        Category("Alignment"),
+        Description("Specifies the alignment of text.")
+        ]
+        public ContentAlignment TextAlignment
+        {
+
+            get
+            {
+                return alignmentValue;
+            }
+            set
+            {
+                alignmentValue = value;
+
+                // The Invalidate method invokes the OnPaint method described 
+                // in step 3.
+                Invalidate();
+            }
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            StringFormat style = new StringFormat();
+            style.Alignment = StringAlignment.Near;
+            switch (alignmentValue)
+            {
+                case ContentAlignment.MiddleLeft:
+                    style.Alignment = StringAlignment.Near;
+                    break;
+                case ContentAlignment.MiddleRight:
+                    style.Alignment = StringAlignment.Far;
+                    break;
+                case ContentAlignment.MiddleCenter:
+                    style.Alignment = StringAlignment.Center;
+                    break;
+            }
+
+            // Call the DrawString method of the System.Drawing class to write   
+            // text. Text and ClientRectangle are properties inherited from
+            // Control.
+            e.Graphics.DrawString(
+                Text,
+                Font,
+                new SolidBrush(ForeColor),
+                ClientRectangle, style);
+
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
+           
+            
+            e.Graphics.DrawEllipse(myPen, new Rectangle(0, 0, 200, 300));
+         
+
+        }
+    }
+}
--- Simple-Form-Control/Form1.Designer.cs
+++ Simple-Form-Control/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flashTrackBar1 = new Microsoft.Samples.WinForms.Cs.FlashTrackBar.FlashTrackBar();
+            this.firstControl1 = new Simple_Form_Control.FirstControl();
+            this.SuspendLayout();
+            // 
+            // flashTrackBar1
+            // 
+            this.flashTrackBar1.BackColor = System.Drawing.Color.Black;
+            this.flashTrackBar1.DarkenBy = ((byte)(218));
+            this.flashTrackBar1.ForeColor = System.Drawing.Color.White;
+            this.flashTrackBar1.Location = new System.Drawing.Point(36, 133);
+            this.flashTrackBar1.Name = "flashTrackBar1";
+            this.flashTrackBar1.Size = new System.Drawing.Size(289, 58);
+            this.flashTrackBar1.TabIndex = 3;
+            this.flashTrackBar1.Text = "flashTrackBar1";
+            this.flashTrackBar1.Value = 72;
+            // 
+            // firstControl1
+            // 
+            this.firstControl1.Location = new System.Drawing.Point(12, 12);
+            this.firstControl1.Name = "firstControl1";
+            this.firstControl1.Size = new System.Drawing.Size(249, 53);
+            this.firstControl1.TabIndex = 2;
+            this.firstControl1.Text = "sss";
+            this.firstControl1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 269);
+            this.Controls.Add(this.flashTrackBar1);
+            this.Controls.Add(this.firstControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private FirstControl firstControl1;
+        private Microsoft.Samples.WinForms.Cs.FlashTrackBar.FlashTrackBar flashTrackBar1;
     }
 }
 
